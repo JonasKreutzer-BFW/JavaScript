7af312947d7d215b61d1619c759176d292c36793
Bergel0n <Jo.Kreutzer@ausbildung.bfwmuc.de>
2026-03-31 12:28:22 +0200

    Add files via upload

---
+++ JavaScriptaufgabe.js
@@ -0,0 +1,7 @@
+function xxxxxx() {    
+  let name = document.getElementById("xxxx").value;
+  document.getElementById("xxxx").innerHTML = "Hallo " + name;
+}
+
+
+//ganze funktion dient nur dazu der variable "name" den gelesenen wert der Eingabe zuzuordnen und am ende auszugeben
